aacd801357334cddf6eec7db01632a940b9108cd
jyothendranath <56503659+jyothendranath@users.noreply.github.com>
2022-04-04 21:52:31 +0530

    changed morgan logs folder

--- middlewares/logs.js
+++ middlewares/logs.js
@@ -6,7 +6,7 @@ const logStream = rfs.createStream("file.log", {
   size: "5M",
   interval: "1d",
   compress: "gzip",
-  path: path.relative(__dirname, 'backend_librarymanagement/logs '),
+  path: path.relative(__dirname, 'backend_librarymangement/logs'),
 });
 
 module.exports = morgan("short", { stream: logStream });
