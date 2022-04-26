0a90b3d6342418d89de955c7af2953b9835595cb
manoj-b-22 <manojsrinivas.b19@iiits.in>
2022-04-27 01:06:20 +0530

    modified mocha testing

--- test/books.js
+++ test/books.js
@@ -3,13 +3,11 @@ const assert = require("assert");
 const app = require("../server");
 const chaiHttp = require("chai-http");
 const { describe } = require("mocha");
-const Book = require("../models/book");
 
 const should = chai.should();
 chai.use(chaiHttp);
 
 describe("Testing Books", function () {
-
   it("get all books", (done) => {
     chai
       .request(app)
@@ -20,58 +18,56 @@ describe("Testing Books", function () {
       });
   });
 
+  let id = ''
+
   it("post book", (done) => {
     let book = {
-      title: 'The Amazing SpiderMan',
-      author: 'Stanlee',
-      ISBN: '970-3-69-148260-0',
-      publication: '2010',
-      link: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rottentomatoes.com%2Fm%2Fthe_amazing_spider_man&psig=AOvVaw1jEPpfBdevPIodyilKN1gd&ust=1651085091458000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPCJxMGxsvcCFQAAAAAdAAAAABAD',
-      addedBy: 'kranthi'
-    }
-    chai.request(app)
-    .post("/books")
-    .send(book)
-    .end((err,res)=>{
-      res.should.have.status(201)
-      done();
-    })
-  })
+      title: "The Amazing SpiderMan",
+      author: "Stanlee",
+      ISBN: "970-3-69-148260-0",
+      publication: "2010",
+      link: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rottentomatoes.com%2Fm%2Fthe_amazing_spider_man&psig=AOvVaw1jEPpfBdevPIodyilKN1gd&ust=1651085091458000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPCJxMGxsvcCFQAAAAAdAAAAABAD",
+      addedBy: "kranthi",
+    };
+    chai
+      .request(app)
+      .post("/books")
+      .send(book)
+      .end((err, res) => {
+        res.should.have.status(201);
+        id = res.body._id
+        done();
+      });
+  });
 
   it("get book by id", (done) => {
-    Book.findOne({ title: "The Amazing SpiderMan" }).then((book) => {
-      chai
-        .request(app)
-        .get("/books/" + book._id)
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
+    chai
+      .request(app)
+      .get("/books/" + id)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
   });
 
   it("patch book", (done) => {
-    Book.findOne({ title: "The Amazing SpiderMan" }).then((book) => {
-      chai
-        .request(app)
-        .patch("/books/" + book._id)
-        .send({ title: "The Chronicles of Narnia" })
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
+    chai
+      .request(app)
+      .patch("/books/" + id)
+      .send({ title: "The Chronicles of Narnia" })
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
   });
 
   it("delete book", (done) => {
-    Book.findOne({ title: "The Chronicles of Narnia" }).then((book) => {
-      chai
-        .request(app)
-        .delete("/books/" + book._id)
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
+    chai
+      .request(app)
+      .delete("/books/" + id)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
   });
 });
